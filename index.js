39950fb9b2c23e4a4370cb8a626c79735e264f9e
Sujeet Gupta <sujeet.g@gleaminnovations.com>
2024-09-02 09:54:57 +0530

    After Add student API some change

--- index.js
+++ index.js
@@ -70,8 +70,6 @@ server.on('error', (err) => {
 //   });
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB Atlas connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
