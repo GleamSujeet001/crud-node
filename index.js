1c0aea3d61877fbe95a867fb8baba6f1407bed29
Sujeet Gupta <sujeet.g@gleaminnovations.com>
2024-09-26 16:12:23 +0530

    after solve issues and port change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const socketIo = require("socket.io");
 require("dotenv").config();
 app.use(express.json());
 const corsOptions = {
-  origin: "*"
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
